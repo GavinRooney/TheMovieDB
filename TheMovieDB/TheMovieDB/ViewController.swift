0e91733489593958f8401c5a208c9fea6c357cbb
Gavin <>
2018-09-05 16:36:34 +0100

    initial project creation

---
+++ TheMovieDB/TheMovieDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by Gavin on 05/09/2018.
+//  Copyright © 2018 Safehouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
