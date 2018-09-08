4715b22bc933cca6dfe139284f86ebb9c6b0e390
Gavin <>
2018-09-08 09:31:03 +0100

    filled in an api test

--- TheMovieDB/TheMovieDBAPITests/TheMovieDBAPITests.swift
+++ TheMovieDB/TheMovieDBAPITests/TheMovieDBAPITests.swift
@@ -9,7 +9,7 @@
 import XCTest
 
 // The following tests are to check the api responses and make sure they are always what we expect. we can quickly see if any server side changes breaks the api / client contract
-// While testing for this is often done on server side I like to also have tests for each api response and models used to keep everyone honest
+// While testing for this is often done on server side, I like to also have tests for each api response and models used. This helps to keep everyone honest
 
 class TheMovieDBAPITests: XCTestCase {
     
@@ -24,12 +24,71 @@ class TheMovieDBAPITests: XCTestCase {
     }
     
     func testGetMoviesAPIResponse() {
-        // given
         
         let moviesWorker = GetMoviesWorker()
         let promise = expectation(description: "Status code: 200")
         
         moviesWorker.request(query: "batman", page: 1, completion: { response in
+            
+            guard response.page != nil else {
+                return
+            }
+            
+            guard response.totalPages != nil else {
+                return
+            }
+            
+            guard response.totalResults != nil else {
+                return
+            }
+            
+            if let results = response.results {
+                if let movie = results.first {
+                    
+                    guard movie.voteCount != nil else {
+                        return
+                    }
+                    guard movie.voteAverage != nil else {
+                        return
+                    }
+                    guard movie.posterPath != nil else {
+                        return
+                    }
+                    guard movie.originalLanguage != nil else {
+                        return
+                    }
+                    guard movie.originalTitle != nil else {
+                        return
+                    }
+                    guard movie.backdropPath != nil else {
+                        return
+                    }
+                    guard movie.overview != nil else {
+                        return
+                    }
+                    guard movie.releaseDate != nil else {
+                        return
+                    }
+                    guard movie.genreIds != nil else {
+                        return
+                    }
+                    guard movie.popularity != nil else {
+                        return
+                    }
+                    guard movie.adult != nil else {
+                        return
+                    }
+                    guard movie.title != nil else {
+                        return
+                    }
+                    guard movie.id != nil else {
+                        return
+                    }
+                    guard movie.video != nil else {
+                        return
+                    }
+                }
+            }
             
             promise.fulfill()
             
@@ -39,12 +98,6 @@ class TheMovieDBAPITests: XCTestCase {
         
         waitForExpectations(timeout: 5, handler: nil)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+
     
 }
